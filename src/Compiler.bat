f6d693c8f4101debf5f908a8eb9016f4e903cbc4
Michi The Cat <mrzombsoriginal@gmail.com>
2026-04-02 20:01:58 +0300

    0.3d-2

--- src/Compiler.bat
+++ src/Compiler.bat
@@ -8,13 +8,11 @@ chcp 65001 > nul
 gcc %~dp0GreenTerminal.c -o %~dp0..\bin\windows\GreenTerminal.exe
 gcc -static %~dp0GreenTerminal.c -o %~dp0..\bin\windows\GreenTerminalStatic.exe
 gcc -S %~dp0GreenTerminal.c -o %~dp0..\bin\windows\GreenTerminal.s
-gcc -S -static %~dp0GreenTerminal.c -o %~dp0..\bin\windows\GreenTerminalStatic.s
 
 :: TODO make Linux Bash аналог этого файла
 :: x86_64-linux-gnu-gcc %~dp0GreenTerminal.c -o %~dp0..\bin\linux\GreenTerminal
 :: x86_64-linux-gnu-gcc -static %~dp0GreenTerminal.c -o %~dp0..\bin\linux\GreenTerminalStatic
 :: x86_64-linux-gnu-gcc -S %~dp0GreenTerminal.c -o %~dp0..\bin\linux\GreenTerminal.s
-:: x86_64-linux-gnu-gcc -S -static %~dp0GreenTerminal.c -o %~dp0..\bin\linux\GreenTerminalStatic.s
 
 echo --------------------------------------------
 echo  Всё установлено! Если не вылезла ошибка...
